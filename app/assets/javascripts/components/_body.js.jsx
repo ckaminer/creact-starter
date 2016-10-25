7b28ce6a5214a25731e1df9b83127f8fd70360d6
Charles Kaminer <ckamine@g.clemson.edu>
2016-10-25 17:29:52 -0600

    complete through iteration 7 now able to add new skills without refresh moved skills state to body and pass it to all skills as prop handleSubmit action written in body handleSubmit passed to new skill as prop add new skill from to new skill component ajax lives in new skill component

--- app/assets/javascripts/components/_body.js.jsx
+++ app/assets/javascripts/components/_body.js.jsx
@@ -1,12 +1,25 @@
 var Body = React.createClass({
+  getInitialState() {
+    return { skills: [] }
+  },
+
+  componentDidMount() {
+    $.getJSON('/api/v1/skills.json', (response) => { this.setState({ skills: response }) });
+  },
+
+  handleSubmit(skill) {
+    var newState = this.state.skills.concat(skill);
+    this.setState({ skills: newState })
+  },
+
   render() {
     return (
       <div>
         <div>
-          <AllSkills />
+          <AllSkills skills={this.state.skills} />
         </div>
         <div>
-          <NewSkill />
+          <NewSkill handleSubmit={this.handleSubmit} />
         </div>
       </div>
     )
